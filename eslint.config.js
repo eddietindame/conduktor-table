b6d7fa3863b48d724f29aba35ba4197fbbe13b0c
Eddie Tindame <eddie.tindame@googlemail.com>
2025-03-12 16:22:07 +0000

    feat: import alias & eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,18 +4,29 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier/flat'
+import reactDom from 'eslint-plugin-react-dom'
+import reactx from 'eslint-plugin-react-x'
 
 export default tseslint.config(
   eslintConfigPrettier,
-  { ignores: ['dist'] },
+  { ignores: ['dist', 'coverage'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
+      'react-x': reactx,
+      'react-dom': reactDom,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import viteLogo from '/vite.svg'
 
-import reactLogo from '../assets/react.svg'
-import { cn } from '../lib/utils'
+import reactLogo from '@/assets/react.svg'
+import { cn } from '@/lib/utils'
 import './App.css'
 
 export function App() {
@@ -11,16 +11,17 @@ export function App() {
   return (
     <main className="flex flex-col gap-2">
       <div className="flex justify-center">
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://vite.dev" target="_blank" rel="noopener noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="flex flex-col gap-4 p-8">
         <button
+          type="button"
           onClick={() => setCount(count => count + 1)}
           className={cn('bg-[#1a1a1a]', count > 10 && 'bg-red-500')}
         >

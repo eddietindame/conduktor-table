796132eba2dd53adb0cb03141f9a3a1ff5b82074
Eddie Tindame <eddie.tindame@googlemail.com>
2025-03-12 14:37:32 +0000

    feat: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
+import { cn } from './lib/utils'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -17,8 +19,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
+      <div className="flex flex-col gap-4 p-8">
+        <button
+          onClick={() => setCount(count => count + 1)}
+          className={cn('bg-[#1a1a1a]', count > 10 && 'bg-red-500')}
+        >
           count is {count}
         </button>
         <p>
@@ -28,7 +33,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
